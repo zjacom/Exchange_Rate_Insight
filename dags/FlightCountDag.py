23fe320b4f9f20e3b01ea9d7c0a90c23f1dc651e
Elisha0510 <sjeong35@knu.ac.kr>
2024-06-13 19:38:40 +0900

    feat: flight_count에 대한 DAG 파일 추가

---
+++ dags/FlightCountDag.py
@@ -0,0 +1,118 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.models import Variable
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.models import TaskInstance
+
+from datetime import datetime, timedelta
+import requests
+import logging
+import psycopg2
+import json
+import pendulum
+
+kst = pendulum.timezone("Asia/Seoul")
+
+dag = DAG(
+    dag_id = 'getFlightCountDAG',
+    start_date = datetime(2024,6,12, tzinfo=kst),
+    schedule= None,
+    catchup = False
+)
+
+CREATE_SQL = """
+CREATE TABLE IF NOT EXISTS kyg8821.flight_count (
+    created_at varchar(20),
+    airportCode varchar(5),
+    flightCount varchar(5)
+);
+"""
+
+def extract(**context):
+    logging.info("Extract started")
+    url = context["params"]["url"]
+    airportList = context["params"]["airportName"]
+    data_list = []
+    for name in airportList:
+        params ={'serviceKey' : 'm2jY3ORe50S7ElYkebSddSiVg9TeK/ySwiH4MT2eUm1EXXLNd9aRjZu1Phv57qWBQDr4YzFny/23gir+Egof+g==',
+                'numOfRows' : '100',
+                'pageNo' : '1',
+                'from_time' : '0000',
+                'to_time' : '2400',
+                'airport' : name,
+                'lang' : 'K',
+                'type' : 'json' }
+
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            json_content = response.json()
+            json_data = json.dumps(json_content, indent=4, ensure_ascii=False)
+            data_list.append(json_data)
+        else:
+            logging.info("Error : "+response.status_code)
+
+    logging.info("Extract done")
+    return data_list
+
+def transform(**context):
+    data_list = context['ti'].xcom_pull(task_ids=f'flightCount_extract')
+    logging.info("got extract return value")
+
+    logging.info("Transform started")
+    dict_list = {}
+    for data_item in data_list:
+        trans_data = json.loads(data_item)
+        cnt = str(trans_data["response"]["body"]["totalCount"])
+        code = trans_data["response"]["body"]["items"][0]["airportCode"]
+        dict_list[code] = cnt
+    logging.info("Transform ended")
+    return dict_list
+
+def generate_insert_query(**context):
+
+    dict_list = context['ti'].xcom_pull(task_ids=f'flightCount_transform')
+    today_date = datetime.now().strftime('%Y-%m-%d')
+
+    # pg_hook = PostgresHook(postgres_conn_id="3rd-Project")
+    pg_hook = PostgresHook(postgres_conn_id='redshift_conn_id')
+
+    for key, value in dict_list.items():
+        sql_statement = f"""INSERT INTO kyg8821.flight_count (created_at, airportCode, flightCount) 
+        VALUES ('{today_date}', {key}, {value});"""
+        logging.info(sql_statement)
+        pg_hook.run(sql_statement)
+
+    logging.info("Generate is Finish")
+
+
+# flight_count 테이블 생성
+createFlightCountTable = PostgresOperator(
+    task_id = "create_flightCount_table",
+    # postgres_conn_id = "3rd-Project",
+    postgres_conn_id = 'redshift_conn_id',
+    sql=CREATE_SQL,
+    dag = dag)
+
+flightCountDataExtract = PythonOperator(
+    task_id = 'flightCount_extract',
+    python_callable = extract,
+    params = {
+        'url': 'http://apis.data.go.kr/B551177/StatusOfPassengerWorldWeatherInfo/getPassengerDeparturesWorldWeather',
+        'airportName' : ['KIX', 'NRT', 'FUK', 'BKK', 'HKT', 'CNX', 'SIN', 'SYD', 'BNE', 'LHR']
+    },
+    dag = dag)
+
+flightCountDataTransform = PythonOperator(
+    task_id = 'flightCount_transform',
+    python_callable = transform,
+    provide_context = True,
+    dag = dag)
+
+generateInsertQuery = PythonOperator(
+    task_id = 'generate_insert_query',
+    python_callable = generate_insert_query,
+    provide_context = True,
+    dag = dag)
+
+createFlightCountTable >> flightCountDataExtract >> flightCountDataTransform >> generateInsertQuery
